b562fdd74c19036448a493df2c778ce7e258e4e0
Asian Dude <36437443+YourTypicalAsian@users.noreply.github.com>
2020-04-30 20:50:58 +0200

    sat op??

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pjson = require('./package.json');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+
+
+
+require('./config/index')(app);
+require('./routes/index')(app);
+
+
+app.listen(port, () => {
+    console.log("Det virker måske");
+});
